8454d940ac673c79a03e64a5b085ce1b9e29c7dc
hlccare <280108904@qq.com>
2021-04-23 09:53:52 +0800

    添加Dialog组件

--- src/components/DIalogDemo.vue
+++ src/components/DIalogDemo.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>
-        DialogDemo
-    </div>
+  <div>DialogDemo</div>
+  <h1>示例1</h1>
+  <Dialog></Dialog>
 </template>
+<script lang='ts'>
+import Dialog from "../lib/Dialog.vue";
+export default {
+  components: { Dialog },
+};
+</script>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="aki-dialog-overlay"></div>
+  <div class="dialog-wrapper">
+    <header>标题</header>
+    <main>
+      <p>第一行</p>
+      <p>第二行</p>
+    </main>
+    <footer>
+      <Button>OK</Button>
+      <Button>cancel</Button>
+    </footer>
+  </div>
+</template>
+<script lang='ts'>
+import Button from "../lib/Button.vue";
+export default {
+  components: { Button },
+};
+</script>
