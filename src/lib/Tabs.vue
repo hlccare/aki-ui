7d2bb97135f36e53ccfb8c6b949e8f0e690bbc26
hlccare <280108904@qq.com>
2021-04-24 16:05:35 +0800

    添加下划线并实现滑动效果

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="aki-tabs">
-        <div class="aki-tabs-nav">
-            <div class="aki-tabs-nav-item" v-for="(t,index) in titles" @click="select(t)" :class="{selected:t===selected}" :key="index">{{t}}</div>
+        <div class="aki-tabs-nav" ref="container">
+            <div class="aki-tabs-nav-item" v-for="(t,index) in titles" :ref="el=>{if(el) navItems[index] = el}" @click="select(t)" :class="{selected:t===selected}" :key="index">{{t}}</div>
+            <div class="aki-tabs-nav-indicator" ref='indicator'></div>
         </div>
         <div class="aki-tabs-content">
             <Component class="aki-tabs-content-item" :class="{selected: c.props.title===selected}" v-for='(c,index) in defaults' :key="index" :is='c'/>
@@ -10,6 +11,7 @@
 </template>
 
 <script lang="ts">
+import { onMounted, onUpdated, ref } from 'vue'
 import Tab from '../lib/Tab.vue'
 export default {
     props:{
@@ -18,6 +20,20 @@ export default {
         }
     },
     setup(props, context){
+        const navItems = ref<HTMLDivElement[]>([])
+        const indicator = ref<HTMLDivElement>(null)
+        const container = ref<HTMLDivElement>(null)
+        const x = ()=>{
+            const divs = navItems.value
+            const result = divs.filter(div=>div.classList.contains('selected'))[0]
+            const {width,left:resultLeft} = result.getBoundingClientRect()
+            indicator.value.style.width = width + 'px'
+            const {left:containerLeft} = container.value.getBoundingClientRect() //左起始点
+            const indicatorLeft = resultLeft - containerLeft
+            indicator.value.style.left = indicatorLeft + 'px'
+        }
+        onMounted(x)
+        onUpdated(x)
         const defaults = context.slots.default()
         defaults.forEach((tag)=>{
             if(tag.type !== Tab){
@@ -28,7 +44,7 @@ export default {
         const select = (title:string) =>{
             context.emit('update:selected',title)
         }
-        return {defaults, titles,select}
+        return {navItems,indicator,container,defaults, titles,select}
     }
 }
 </script>
@@ -43,6 +59,7 @@ $border-color: #d9d9d9;
         display: flex;
         color: $color;
         border-bottom: 1px solid $border-color;
+        position: relative;
     
         &-item{
             padding: 8px 0;
@@ -56,6 +73,15 @@ $border-color: #d9d9d9;
             &.selected{
                 color: $blue;
             }
+        }
+        &-indicator{
+            position: absolute;
+            height: 3px;
+            width: 100px;
+            background: $blue;
+            left: 0;
+            bottom: -1px;
+            transition: all 250ms;
         }
     }
     &-content{
