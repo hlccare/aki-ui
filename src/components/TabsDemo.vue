4e1635df33582718709bea17ead5975b5a38e22e
hlccare <280108904@qq.com>
2021-04-24 11:59:26 +0800

    Tabs基本css

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -4,8 +4,8 @@
     </div>
     <h1>实例1</h1>
     <Tabs>
-        <Tab>内容1</Tab>
-        <Tab>内容2</Tab>
+        <Tab title="标题1">内容1</Tab>
+        <Tab title="标题2">内容2</Tab>
     </Tabs>
 </template>
 <script lang="ts">
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,11 @@
 <template>
-    <div>
-        <div v-for="(t,index) in titles" :key="index">{{t}}</div>
-        <Component v-for='(c,index) in defaults' :key="index" :is='c'/>
+    <div class="aki-tabs">
+        <div class="aki-tabs-nav">
+            <div class="aki-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+        </div>
+        <div class="aki-tabs-content">
+            <Component class="aki-tbas-content-item" v-for='(c,index) in defaults' :key="index" :is='c'/>
+        </div>
     </div>
 </template>
 
@@ -15,8 +19,41 @@ export default {
                 throw new Error('Tabs 子标签必须是Tab')
             } 
         })
-        const titles = defaults.forEach((tag)=> tag.props.title)
+        const titles = defaults.map((tag)=> tag.props.title)
+        console.log(defaults[0].props.title)
         return {defaults, titles}
     }
 }
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+
+.aki-tabs{
+    &-nav{
+        display: flex;
+        color: $color;
+        border-bottom: 1px solid $border-color;
+    
+        &-item{
+            padding: 8px 0;
+            margin: 0 16px;
+            cursor: pointer;
+
+            &:first-child{
+                color: $blue;
+            }
+
+            &.selected{
+                color: $blue;
+            }
+        }
+    }
+    &-content{
+        padding: 8px 0;
+    }
+}
+
+</style>
