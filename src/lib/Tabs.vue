2938895a2422afcf6ea012e52370db8f2ee05dac
hlccare <280108904@qq.com>
2021-04-24 11:45:31 +0800

    检查Tabs子组件类型，渲染Tabs

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,3 +1,22 @@
 <template>
-    <div>Tabs</div>
+    <div>
+        <div v-for="(t,index) in titles" :key="index">{{t}}</div>
+        <Component v-for='(c,index) in defaults' :key="index" :is='c'/>
+    </div>
 </template>
+
+<script lang="ts">
+import Tab from '../lib/Tab.vue'
+export default {
+    setup(props, context){
+        const defaults = context.slots.default()
+        defaults.forEach((tag)=>{
+            if(tag.type !== Tab){
+                throw new Error('Tabs 子标签必须是Tab')
+            } 
+        })
+        const titles = defaults.forEach((tag)=> tag.props.title)
+        return {defaults, titles}
+    }
+}
+</script>
