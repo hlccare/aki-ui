8470a1df0d25a5c82406c06a2ad736c93d08c83c
hlccare <280108904@qq.com>
2021-04-25 10:36:07 +0800

    添加文档相关路由与视图，实现高亮当前路由

--- src/router.ts
+++ src/router.ts
@@ -6,22 +6,30 @@ import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
 import DocDemo from './components/DocDemo.vue'
+import Intro from './views/Intro.vue'
+import GetStarted from './views/GetStarted.vue'
+import Install from './views/Install.vue'
 
 const history = createWebHashHistory()
 export const router = createRouter({
     history,
     routes: [
         { path: '/', component: Home },
-        { path: '/doc', 
-        component: Doc,
-        children:[
-            {path:'',component:DocDemo},
-            {path:'switch',component:SwitchDemo},
-            {path:'button',component:ButtonDemo},
-            {path:'dialog',component:DialogDemo},
-            {path:'tabs',component:TabsDemo},
+        {
+            path: '/doc',
+            component: Doc,
+            children: [
+                { path: '', component: DocDemo },
+                { path: 'get-started', component: GetStarted },
+                { path: 'install', component: Install },
+                { path: 'intro', component: Intro },
+                { path: 'switch', component: SwitchDemo },
+                { path: 'button', component: ButtonDemo },
+                { path: 'dialog', component: DialogDemo },
+                { path: 'tabs', component: TabsDemo },
 
-        ] }
+            ]
+        }
     ]
 })
 router.afterEach
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -3,6 +3,18 @@
     <Topnav :toggleMenuButtonVisible="true" class="nav" />
     <div class="content">
       <aside v-if="asideVisible">
+        <h2>文档</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/intro">文档</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/install">安装</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/get-started">开始使用</router-link>
+          </li>
+        </ol>
         <h2>组件列表</h2>
         <ol>
           <li>
@@ -24,7 +36,7 @@
   </div>
 </template>
 <script lang="ts">
-import { inject, Ref } from "vue";
+import { inject, Ref, h } from "vue";
 import Topnav from "../components/Topnav.vue";
 export default {
   components: { Topnav },
@@ -65,7 +77,7 @@ export default {
 aside {
   background: lightblue;
   width: 150px;
-  padding: 16px;
+  padding: 16px 0;
   position: fixed;
   top: 0;
   left: 0;
@@ -73,10 +85,17 @@ aside {
   height: 100%;
   > h2 {
     margin-bottom: 4px;
+    padding: 0 16px;
   }
   > ol {
     > li {
-      padding: 4px 0;
+      > a {
+        display: block;
+        padding: 4px 16px;
+      }
+      .router-link-active {
+        background: white;
+      }
     }
   }
 }
