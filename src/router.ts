2e90e8e5c4a28b9cb1c814fa7816bbbfb7cad419
hlccare <280108904@qq.com>
2021-04-26 17:27:24 +0800

    修复打包时动态路径问题，增加部署脚本

--- src/router.ts
+++ src/router.ts
@@ -7,10 +7,13 @@ import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
 import Markdown from './components/Markdown.vue'
 import { h } from 'vue';
+import intro from './markdown/intro.md'
+import getStarted from './markdown/getStarted.md'
+import install from './markdown/install.md'
 
 
 const history = createWebHashHistory()
-const md = filename => h(Markdown, { path: `../markdown/${filename}.md`, key: filename })
+const md = string => h(Markdown, { content: string, key: string })
 export const router = createRouter({
     history,
     routes: [
@@ -20,9 +23,9 @@ export const router = createRouter({
             component: Doc,
             children: [
                 { path: '', redirect: '/doc/intro' },
-                { path: 'get-started', component: md('getStarted') },
-                { path: 'install', component: md('install') },
-                { path: 'intro', component: md('intro') },
+                { path: 'get-started', component: md(getStarted) },
+                { path: 'install', component: md(install) },
+                { path: 'intro', component: md(intro) },
                 { path: 'switch', component: SwitchDemo },
                 { path: 'button', component: ButtonDemo },
                 { path: 'dialog', component: DialogDemo },
