bfe87ca3d500c25aa5c583df6fa603cfd169f506
hlccare <280108904@qq.com>
2021-04-26 23:54:01 +0800

    修改DialogDemo

--- src/components/DIalogDemo.vue
+++ src/components/DIalogDemo.vue
@@ -1,54 +1,20 @@
 <template>
-  <div>DialogDemo</div>
-  <h1>示例1</h1>
-  <Button @click="toggle">toggle</Button>
-  <Dialog
-    v-model:visible="x"
-    :closeOnClickOverlay="false"
-    :ok="f1"
-    :cancel="f2"
-  >
-    <template v-slot:title>
-      <strong>加粗标题</strong>
-    </template>
-    <template v-slot:content>
-      <div>1</div>
-      <div>2</div>
-    </template>
-  </Dialog>
-  <h1>示例2</h1>
-  <Button @click="show">show</Button>
+<div>
+    <div>Dialog 示例</div>
+    <Demo :component="Dialog1Demo" />
+    <Demo :component="Dialog2Demo" />
+  </div>
 </template>
 <script lang='ts'>
-import Dialog from "../lib/Dialog.vue";
-import Button from "../lib/Button.vue";
-import { ref } from "vue";
-import { openDialog } from "../lib/openDialog";
+import Dialog1Demo from './Dialog1.demo.vue'
+import Dialog2Demo from './Dialog2.demo.vue'
+import Demo from './Demo.vue';
 export default {
-  components: { Dialog, Button },
-  setup() {
-    const x = ref(false);
-    const toggle = () => {
-      x.value = !x.value;
-    };
-    const f1 = () => {
-      return false;
-    };
-    const f2 = () => {};
-    const show = () => {
-      openDialog({
-        title: () => "标题",
-        content: () => "你好",
-        ok: () => {
-          console.log("ok");
-        },
-        cancel: () => {
-          console.log("cancel");
-        },
-        closeOnClickOverlay: false,
-      });
-    };
-    return { x, toggle, f1, f2, show };
-  },
+  components: { Dialog1Demo,Dialog2Demo, Demo },
+  setup(){
+    return{
+      Dialog1Demo,Dialog2Demo
+    }
+  }
 };
 </script>
---
+++ src/components/Dialog1.demo.vue
@@ -0,0 +1,41 @@
+<demo>
+常规使用
+</demo>
+<template>
+<div>
+    <Button @click="toggle">toggle</Button>
+  <Dialog
+    v-model:visible="x"
+    :closeOnClickOverlay="false"
+    :ok="f1"
+    :cancel="f2"
+  >
+    <template v-slot:title>
+      <strong>加粗标题</strong>
+    </template>
+    <template v-slot:content>
+      <div>1</div>
+      <div>2</div>
+    </template>
+  </Dialog>
+</div>
+</template>
+<script lang="ts">
+import Button from '../lib/Button.vue'
+import Dialog from '../lib/Dialog.vue'
+import { ref } from "vue";
+export default {
+  components: { Dialog, Button },
+  setup() {
+    const x = ref(false);
+    const toggle = () => {
+      x.value = !x.value;
+    };
+    const f1 = () => {
+      return false;
+    };
+    const f2 = () => {};
+    return { x, toggle, f1, f2 };
+  },
+};
+</script>
