6cef81fa5419a2675653fc6f7d0b930d0cecd9fe
hlccare <280108904@qq.com>
2021-04-26 15:21:58 +0800

    修复bug

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,102 +1,114 @@
 <template>
-<div>
-
-  <div class="topnavAndBanner">
-    <Topnav/>
-    <div class="banner">
-      <h1>Aki UI</h1>
-      <h2>一个框架</h2>
-      <p class="actions">
-        <a href="https://github.com">Github</a>
-        <router-link to='/doc'>开始</router-link>
-      </p>
+  <div>
+    <div class="topnavAndBanner">
+      <Topnav />
+      <div class="banner">
+        <h1>Aki UI</h1>
+        <h2>一个框架</h2>
+        <p class="actions">
+          <a href="https://github.com">Github</a>
+          <router-link to="/doc">开始</router-link>
+        </p>
+      </div>
+    </div>
+    <div class="features">
+      <ul>
+        <li>
+          <svg class="icon" aria-hidden="true">
+            <use xlink:href="#icon-vue"></use>
+          </svg>
+          <h3>基于 Vue3</h3>
+          <p>使用了 Vue3 composition API</p>
+        </li>
+        <li>
+          <svg class="icon" aria-hidden="true">
+            <use xlink:href="#icon-ts"></use>
+          </svg>
+          <h3>基于 Typescript</h3>
+          <p>源代码采用 Typescript 书写</p>
+        </li>
+        <li>
+          <svg class="icon" aria-hidden="true">
+            <use xlink:href="#icon-light"></use>
+          </svg>
+          <h3>代码易读</h3>
+          <p>组件代码简介易读</p>
+        </li>
+      </ul>
     </div>
   </div>
-  <div class="features">
-    <ul>
-      <li>
-        <svg class="icon" aria-hidden="true">
-          <use xlink:href="#icon-vue"></use>
-        </svg>
-        <h3>基于 Vue3</h3>
-        <p>使用了 Vue3 composition API</p>
-      </li>
-      <li>
-        <svg class="icon" aria-hidden="true">
-          <use xlink:href="#icon-ts"></use>
-        </svg>
-        <h3>基于 Typescript</h3>
-        <p>源代码采用 Typescript 书写（非严格模式）</p>
-      </li>
-      <li>
-        <svg class="icon" aria-hidden="true">
-          <use xlink:href="#icon-light"></use>
-        </svg>
-        <h3>代码易读</h3>
-        <p>组件代码简介易读</p>
-      </li>
-    </ul>
-  </div>
-</div>
 </template>
 <script lang="ts">
-import Topnav from '../components/Topnav.vue'
-export default{
-  components:{Topnav}
-}
+import Topnav from "../components/Topnav.vue";
+export default {
+  components: { Topnav },
+};
 </script>
 <style lang="scss" scoped>
 $green: #02bcb0;
 $border-radius: 4px;
 $color: #007974;
-.topnavAndBanner{
-  background: rgb(227,255,253);
-  background: linear-gradient(145deg, rgba(227,255,253,1) 0%, rgba(183,233,230,1) 100%);
+.topnavAndBanner {
+  background: rgb(227, 255, 253);
+  background: linear-gradient(
+    145deg,
+    rgba(227, 255, 253, 1) 0%,
+    rgba(183, 233, 230, 1) 100%
+  );
   clip-path: ellipse(80% 50% at 50% 40%);
 }
-.features{
+.features {
   margin: 64px auto;
-  width: 400px;
+  padding: 0 16px;
 
   @media (min-width: 800px) {
     width: 800px;
+
+    > ul {
+      > li {
+        width: 50%;
+      }
+    }
   }
 
-  @media (min-width:1200px) {
+  @media (min-width: 1200px) {
     width: 1200px;
+    > ul {
+      > li {
+        width: 33.3333%;
+      }
+    }
   }
 
-  >ul{
+  > ul {
     display: flex;
     flex-wrap: wrap;
 
-    >li{
-      width: 400px;
+    > li {
       margin: 16px 0;
       display: grid;
       justify-content: start;
       align-content: space-between;
-      grid-template-areas: 
+      grid-template-areas:
         "icon title"
         "icon text";
       grid-template-columns: 80px auto;
       grid-template-rows: 1fr auto;
 
-      >svg{
+      > svg {
         grid-area: icon;
         width: 64px;
         height: 64px;
       }
-      
-      >h3{
+
+      > h3 {
         grid-area: title;
         font-size: 28px;
       }
 
-      >p{
+      > p {
         grid-area: text;
       }
-
     }
   }
 }
@@ -107,16 +119,16 @@ $color: #007974;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  > .actions{
+  > .actions {
     padding: 8px 0;
     a {
-      margin:0 8px;
+      margin: 0 8px;
       background: $green;
       color: white;
       display: inline-block;
       padding: 8px 24px;
       border-radius: $border-radius;
-      &:hover{
+      &:hover {
         text-decoration: none;
       }
     }
