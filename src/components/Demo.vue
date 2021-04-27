f06b3f5cf4f344db95651209c753c5f2c7742597
hlccare <280108904@qq.com>
2021-04-27 17:22:21 +0800

    添加rollup设置文件

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -5,7 +5,8 @@
       <component :is="component" />
     </div>
     <div class="demo-actions">
-      <Button @click="toggleCodeVisible">查看代码</Button>
+      <Button @click="hideCode" v-if="codeVisible">隐藏代码</Button>
+      <Button @click="showCode" v-else>查看代码</Button>
     </div>
     <div class="demo-code" v-if="codeVisible">
       <pre class="language-html" v-html="html" />
@@ -36,10 +37,13 @@ export default {
       );
     });
     const codeVisible = ref(false);
-    const toggleCodeVisible = () => {
-      codeVisible.value = !codeVisible.value;
+    const showCode = () => {
+      codeVisible.value = true;
     };
-    return { Prism, html, codeVisible, toggleCodeVisible };
+    const hideCode = () => {
+      codeVisible.value = false;
+    };
+    return { Prism, html, codeVisible, showCode, hideCode };
   },
 };
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,51 +1,71 @@
 <template>
-    <div class="aki-tabs">
-        <div class="aki-tabs-nav" ref="container">
-            <div class="aki-tabs-nav-item" v-for="(t,index) in titles" :ref="el=>{if(t===selected) selectedItem = el}" @click="select(t)" :class="{selected:t===selected}" :key="index">{{t}}</div>
-            <div class="aki-tabs-nav-indicator" ref='indicator'></div>
-        </div>
-        <div class="aki-tabs-content">
-            <Component class="aki-tabs-content-item" :class="{selected: c.props.title===selected}" v-for='(c,index) in defaults' :key="index" :is='c'/>
-        </div>
+  <div class="aki-tabs">
+    <div class="aki-tabs-nav" ref="container">
+      <div
+        class="aki-tabs-nav-item"
+        v-for="(t, index) in titles"
+        :ref="
+          (el) => {
+            if (t === selected) selectedItem = el;
+          }
+        "
+        @click="select(t)"
+        :class="{ selected: t === selected }"
+        :key="index"
+      >
+        {{ t }}
+      </div>
+      <div class="aki-tabs-nav-indicator" ref="indicator"></div>
     </div>
+    <div class="aki-tabs-content">
+      <Component
+        class="aki-tabs-content-item"
+        :class="{ selected: c.props.title === selected }"
+        v-for="(c, index) in defaults"
+        :key="index"
+        :is="c"
+      />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { onMounted, onUpdated, ref, watchEffect } from 'vue'
-import Tab from '../lib/Tab.vue'
+import { onMounted, onUpdated, ref, watchEffect } from "vue";
+import Tab from "../lib/Tab.vue";
 export default {
-    props:{
-        selected:{
-            type:String,
-        }
+  props: {
+    selected: {
+      type: String,
     },
-    setup(props, context){
-        const selectedItem = ref<HTMLDivElement>(null)
-        const indicator = ref<HTMLDivElement>(null)
-        const container = ref<HTMLDivElement>(null)
-        const x = ()=>{
-            const {width,left:resultLeft} = selectedItem.value.getBoundingClientRect()
-            indicator.value.style.width = width + 'px'
-            const {left:containerLeft} = container.value.getBoundingClientRect()
-            const indicatorLeft = resultLeft - containerLeft
-            indicator.value.style.left = indicatorLeft + 'px'
-        }
-        onMounted(
-            ()=>watchEffect(x)
-        )
-        const defaults = context.slots.default()
-        defaults.forEach((tag)=>{
-            if(tag.type !== Tab){
-                throw new Error('Tabs 子标签必须是Tab')
-            } 
-        })
-        const titles = defaults.map((tag)=> tag.props.title)
-        const select = (title:string) =>{
-            context.emit('update:selected',title)
-        }
-        return {selectedItem,indicator,container,defaults, titles,select}
-    }
-}
+  },
+  setup(props, context) {
+    const selectedItem = ref<HTMLDivElement>(null);
+    const indicator = ref<HTMLDivElement>(null);
+    const container = ref<HTMLDivElement>(null);
+    const x = () => {
+      const {
+        width,
+        left: resultLeft,
+      } = selectedItem.value.getBoundingClientRect();
+      indicator.value.style.width = width + "px";
+      const { left: containerLeft } = container.value.getBoundingClientRect();
+      const indicatorLeft = resultLeft - containerLeft;
+      indicator.value.style.left = indicatorLeft + "px";
+    };
+    onMounted(() => watchEffect(x));
+    const defaults = context.slots.default();
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error("Tabs 子标签必须是Tab");
+      }
+    });
+    const titles = defaults.map((tag) => tag.props.title);
+    const select = (title: String) => {
+      context.emit("update:selected", title);
+    };
+    return { selectedItem, indicator, container, defaults, titles, select };
+  },
+};
 </script>
 
 <style lang="scss">
@@ -53,45 +73,44 @@ $blue: #40a9ff;
 $color: #333;
 $border-color: #d9d9d9;
 
-.aki-tabs{
-    &-nav{
-        display: flex;
-        color: $color;
-        border-bottom: 1px solid $border-color;
-        position: relative;
-    
-        &-item{
-            padding: 8px 0;
-            margin: 0 16px;
-            cursor: pointer;
+.aki-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    position: relative;
 
-            &:first-child{
-                margin-left: 0;
-            }
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
 
-            &.selected{
-                color: $blue;
-            }
-        }
-        &-indicator{
-            position: absolute;
-            height: 3px;
-            background: $blue;
-            left: 0;
-            bottom: -1px;
-            transition: all 250ms;
-        }
+      &:first-child {
+        margin-left: 0;
+      }
+
+      &.selected {
+        color: $blue;
+      }
     }
-    &-content{
-        padding: 8px 0;
-        &-item{
-            display: none;
-
-            &.selected{
-                display: block;
-            }
-        }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      transition: all 250ms;
     }
+  }
+  &-content {
+    padding: 8px 0;
+    &-item {
+      display: none;
+
+      &.selected {
+        display: block;
+      }
+    }
+  }
 }
-
 </style>
